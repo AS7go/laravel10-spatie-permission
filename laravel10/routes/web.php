c3df5baf6112679241e62862d8981bb0bdcc58f9
Aleksandr <sh17aleksandr@gmail.com>
2024-05-20 19:40:04 +0300

    update delete

--- laravel10/routes/web.php
+++ laravel10/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
+// use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +21,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('add-post', [PostController::class, 'create'])->name('add-post1');
-Route::post('store-post', [PostController::class, 'store'])->name('store-post1');
-Route::get('edit-post/{id}', [PostController::class, 'edit'])->name('edit-post1');
-Route::put('update-post/{id}', [PostController::class, 'update'])->name('update-post1');
-Route::delete('delete-post/{id}', [PostController::class, 'delete'])->name('delete-post1');
-
+Route::middleware(['auth'])->group(function () {
+    // Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
+    
+    Route::get('add-post', [PostController::class, 'create'])->name('add-post1');
+    Route::post('store-post', [PostController::class, 'store'])->name('store-post1');
+    Route::get('edit-post/{id}', [PostController::class, 'edit'])->name('edit-post1');
+    Route::put('update-post/{id}', [PostController::class, 'update'])->name('update-post1');
+    Route::delete('delete-post/{id}', [PostController::class, 'delete'])->name('delete-post1');
+    
+    Route::resource('roles', RoleController::class);
+});
 
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,4 +40,4 @@ Route::delete('delete-post/{id}', [PostController::class, 'delete'])->name('dele
 //     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
